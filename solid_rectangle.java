7fc4c6bf3cf75312d559081d9775fd09abc82184
Omkar9911 <omkarj.9900@gmail.com>
2023-10-26 14:09:44 +0530

    1st Q added

---
+++ solid_rectangle.java
@@ -0,0 +1,32 @@
+  //Q 1. Solid rectangle star
+  //        * * * * *
+  //        * * * * *
+  //        * * * * *
+  //        * * * * *
+
+
+  import java.util.*;
+
+  public class solid_rectangle
+  {
+    public static void main(String args[])
+    
+    {
+      Scanner sc = new Scanner(System.in); 
+  
+      System.out.println("Enter no of rows = ");
+      int rows = sc.nextInt();
+      System.out.println("Enter no of coloumns = ");
+      int col = sc.nextInt();
+  
+      for(int i=1; i<=rows; i++)
+      {
+        for(int j=1; j<=col; j++)
+        {
+          System.out.print(" *");
+        }
+        System.out.println();
+      }
+  
+    }
+  }
