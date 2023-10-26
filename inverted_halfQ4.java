f22e4289dfccdd3a88a6191395a8254b8783dc1e
Omkar9911 <omkarj.9900@gmail.com>
2023-10-26 15:30:14 +0530

    4 Q Added

---
+++ inverted_halfQ4.java
@@ -0,0 +1,27 @@
+//Q 1. Solid rectangle star
+  //        * * * * *
+  //        * * * * 
+  //        * * * 
+  //        * * 
+  //        *
+  
+import java.util.*;
+public class inverted_halfQ4 
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter size of pyramid = ");
+    int n =sc.nextInt();
+
+    for(int i=1; i<=n; i++)
+    {
+      for(int j=n; j>=i; j--)
+      {
+        System.out.print(" *");
+      }
+      System.out.println();
+    }
+
+  }
+}
