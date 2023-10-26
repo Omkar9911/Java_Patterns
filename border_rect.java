bbc88cc8b857c26fb789df7693609a3f9c9c5cc0
Omkar9911 <omkarj.9900@gmail.com>
2023-10-26 14:50:18 +0530

    2nd Q Added

---
+++ border_rect.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class border_rect
+{
+  public static void main(String args[])
+  {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter no of rows = ");
+    int row = sc.nextInt();
+    System.out.println("Enter no of coloumns = ");
+    int col = sc.nextInt();
+
+    for(int i=1; i<=row; i++)
+    {
+      for(int j=1; j<=col; j++)
+      {
+        if(i==1 || i==row || j==1 || j==col)
+        {
+          System.out.print( " *");
+        }else
+        {
+          System.out.print( "  ");
+        }
+      }
+      System.out.println();
+    }
+
+  }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,14 @@
+for(int i=1; i<=row; i++)
+    // {
+    //   for(int j=1; j<=col; j++)
+    //   {
+    //     if(i=1 || i=row || j=1 || j=col)
+    //     {
+    //       System.out.print( "*");
+    //     }eles
+    //     {
+    //       System.out.print( " ");
+    //     }
+    //   }
+    //   System.out.println();
+    // }
